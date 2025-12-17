3d8bd3a1da084da62aae642cbea9e2ed6d81ec95
Mahdi Malekpour <mahdi.me.14.m@gmail.com>
2025-12-17 12:26:10 -0800

    Prepare static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // برای static export
-  images: {
-    unoptimized: true, // <--- این گزینه Image Optimization رو خاموش می‌کنه
-  },
+  output: 'export',           // برای ساخت static
+  images: { unoptimized: true }, // حذف Image Optimization
+  basePath: '/yab-market',    // اسم ریپو روی GitHub
+  assetPrefix: '/yab-market/', // مسیر asset ها
 };
 
 module.exports = nextConfig;
